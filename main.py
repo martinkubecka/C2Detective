cac3f11b83a6b3681c8affe4704a5e59b901d96e
Martin Kubečka <martin.kubecka98@gmail.com>
2022-09-27 23:49:05 +0200

    added argument parser

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import argparse
+import sys
+import os
+
+
 def banner():
     print(r"""
    ____ ____  ____       _            _   _           
@@ -11,8 +16,39 @@ def banner():
     """)
 
 
+def is_valid_file(file):
+    if not os.path.exists(file):
+        print(f"[!] Provided file '{file}' does not exist.")
+        exit(1)
+    # else:
+    #     return open(arg, 'r')  # return an open file handle
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog='c2detective',
+                                     description='Application for detecting command and control (C2) '
+                                                 'communication through network traffic analysis.')
+
+    parser.add_argument('-q', '--quiet', help="don't print the banner and other noise", action='store_true')
+    parser.add_argument('-i', help='input file (.cap OR .pcap)', metavar='FILE', required=True,
+                        type=lambda file: is_valid_file(file))
+
+    return parser.parse_args()
+
+
 def main():
-    banner()
+    args = parse_arguments()
+    # interactive mode : sys.argv[0] == main.py
+    if len(sys.argv) == 1:
+        print("interactive mode")
+        banner()
+    # command line mode
+    else:
+        print("command line mode")
+        if not args.quiet:
+            banner()
+        if args.input:
+            print("input set")
 
 
 if __name__ == '__main__':
