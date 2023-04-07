80f665804fe5ea2ed4b96ccfeff14cb653254a9d
Martin Kubečka <martin.kubecka98@gmail.com>
2023-04-07 12:21:39 +0200

    added thorough configuration file checks

--- iocs/tor/update_tor_nodes.py
+++ iocs/tor/update_tor_nodes.py
@@ -14,46 +14,39 @@ class TorNodes:
         self.tor_iocs_path = f"{os.path.dirname(os.path.realpath(sys.argv[0]))}/iocs/tor/tor_nodes.json"
         
     def update_tor_nodes(self):
-        if self.url_tor_node_list and self.url_tor_exit_nodes:
-            # fetching all nodes
-            print(f"[{time.strftime('%H:%M:%S')}] [INFO] Fetching the most up-to-date Tor node list ...")
-            logging.info(f"Fetching the most up-to-date Tor node list ...")
-            response = requests.get(self.url_tor_node_list)
-            if response.status_code == 200:
-                data = response.text
-                all_nodes = data.splitlines()
-            else:
-                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Error ocurred while fetching Tor node list")
-                logging.error(f"Error ocurred while fetching Tor node list", exc_info=True)
-                all_nodes = []
-
-            # fetching exit nodes
-            print(f"[{time.strftime('%H:%M:%S')}] [INFO] Fetching the most up-to-date Tor exit node list ...")
-            logging.info(f"Fetching the most up-to-date Tor exit node list ...")
-            response = requests.get(self.url_tor_exit_nodes)
-            if response.status_code == 200:
-                data = response.text
-                exit_nodes = data.splitlines()
-            else:
-                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Error ocurred while fetching Tor node list")
-                logging.error(f"Error ocurred while fetching Tor node list", exc_info=True)
-                exit_nodes = []
-
-            # caching fetched Tor nodes
-            if not all_nodes or not exit_nodes:
-                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Retrieving Tor IOCs was not successful")
-                logging.error(f"Retrieving Tor IOCs was not successful")
-                print("\nExiting program ...\n")
-                sys.exit(1)
-            else:    
-                print(f"[{time.strftime('%H:%M:%S')}] [INFO] Caching fetched Tor exit node lists ...")
-                logging.info(f"Caching fetched Tor exit node lists")
-                tor_nodes = {'all_nodes': all_nodes, 'exit_nodes': exit_nodes}
-                data = json.dumps(tor_nodes, indent=4)
-                with open(self.tor_iocs_path, "w") as output:
-                    output.write(data)
+        # fetching all nodes
+        print(f"[{time.strftime('%H:%M:%S')}] [INFO] Fetching the most up-to-date Tor node list ...")
+        logging.info(f"Fetching the most up-to-date Tor node list ...")
+        response = requests.get(self.url_tor_node_list)
+        if response.status_code == 200:
+            data = response.text
+            all_nodes = data.splitlines()
         else:
-            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] URLs for Tor node lists are not present in the config ...")
-            logging.error(f"URLs for Tor node lists are not present in the config")
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Error ocurred while fetching Tor node list")
+            logging.error(f"Error ocurred while fetching Tor node list", exc_info=True)
+            all_nodes = []
+
+        # fetching exit nodes
+        print(f"[{time.strftime('%H:%M:%S')}] [INFO] Fetching the most up-to-date Tor exit node list ...")
+        logging.info(f"Fetching the most up-to-date Tor exit node list ...")
+        response = requests.get(self.url_tor_exit_nodes)
+        if response.status_code == 200:
+            data = response.text
+            exit_nodes = data.splitlines()
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Error ocurred while fetching Tor node list")
+            logging.error(f"Error ocurred while fetching Tor node list", exc_info=True)
+            exit_nodes = []
+        # caching fetched Tor nodes
+        if not all_nodes or not exit_nodes:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Retrieving Tor IOCs was not successful")
+            logging.error(f"Retrieving Tor IOCs was not successful")
             print("\nExiting program ...\n")
             sys.exit(1)
+        else:    
+            print(f"[{time.strftime('%H:%M:%S')}] [INFO] Caching fetched Tor exit node lists ...")
+            logging.info(f"Caching fetched Tor exit node lists")
+            tor_nodes = {'all_nodes': all_nodes, 'exit_nodes': exit_nodes}
+            data = json.dumps(tor_nodes, indent=4)
+            with open(self.tor_iocs_path, "w") as output:
+                output.write(data)
--- src/analyst_profile.py
+++ src/analyst_profile.py
@@ -1,55 +1,131 @@
+import logging
+import time
+import sys
+
 class AnalystProfile:
     def __init__(self, config):
-        # TODO: add checks
+
         self.name = config.get('name')
+        if self.name is None:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain the name of the analysis ...")
+            logging.error(f"The configuration file does not contain the name of the analysis")
+            print("\nExiting program ...\n")
+            sys.exit(1)
 
-        # TODO: add checks
         self.api_keys = config.get('api_keys')
-        self.abuseipdb_api_key = self.api_keys.get('abuseipdb')
-        self.virustotal_api_key = self.api_keys.get('virustotal')
-        self.securitytrails_api_key = self.api_keys.get('securitytrails')
-        self.shodan_api_key = self.api_keys.get('shodan')
+        if self.api_keys:
+            self.abuseipdb_api_key = self.api_keys.get('abuseipdb')
+            self.virustotal_api_key = self.api_keys.get('virustotal')
+            self.securitytrails_api_key = self.api_keys.get('securitytrails')
+            self.shodan_api_key = self.api_keys.get('shodan')
 
-        # TODO: add checks
+            if any(api_key is None for api_key in (self.abuseipdb_api_key, self.virustotal_api_key, self.securitytrails_api_key, self.shodan_api_key)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Incomplete API keys are present in the configuration file ...")
+                logging.error(f"Incomplete API keys are present in the configuration file")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] API keys are not present in the configuration file ...")
+            logging.error(f"API keys are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
         self.settings = config.get('settings')
-        self.statistics_top_count = self.settings.get('statistics_top_count')
-        self.chunk_size = self.settings.get('chunk_size')
+        if self.settings:
+            self.statistics_top_count = self.settings.get('statistics_top_count')
+            self.chunk_size = self.settings.get('chunk_size')
 
-        # checks in place 
+            if any(setting is None for setting in (self.statistics_top_count, self.chunk_size)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain general settings ...")
+                logging.error(f"The configuration file does not contain general settings")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Settings are not present in the configuration file ...")
+            logging.error(f"Settings are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
         self.feeds = config.get('feeds')
-        self.tor_node_list = self.feeds.get('tor_node_list')
-        self.tor_exit_node_list = self.feeds.get('tor_exit_node_list')
-        self.crypto_domains = self.feeds.get('crypto_domains')
-        self.ja3_rules = self.feeds.get('ja3_rules')
+        if self.feeds:
+            self.tor_node_list = self.feeds.get('tor_node_list')
+            self.tor_exit_node_list = self.feeds.get('tor_exit_node_list')
+            self.crypto_domains = self.feeds.get('crypto_domains')
+            self.ja3_rules = self.feeds.get('ja3_rules')
 
-        # TODO: add checks 
+            if any(feed is None for feed in (self.tor_node_list, self.tor_exit_node_list, self.crypto_domains, self.ja3_rules)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain complete URLs for feeds ...")
+                logging.error(f"The configuration file does not contain complete URLs for feeds")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Feed URLs are not present in the configuration file ...")
+            logging.error(f"Feed URLs are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
         self.enrichment_services = config.get('enrichment_services')
-        self.abuseipdb = self.enrichment_services.get('abuseipdb')
-        self.threatfox = self.enrichment_services.get('threatfox')
-        self.securitytrails = self.enrichment_services.get('securitytrails')
-        self.virustotal = self.enrichment_services.get('virustotal')
-        self.shodan = self.enrichment_services.get('shodan')
-        self.alienvault = self.enrichment_services.get('alienvault')
-        self.bgp_ranking = self.enrichment_services.get('bgp_ranking')
-        self.urlhaus = self.enrichment_services.get('urlhaus')
+        if self.enrichment_services:
+            self.abuseipdb = self.enrichment_services.get('abuseipdb')
+            self.threatfox = self.enrichment_services.get('threatfox')
+            self.securitytrails = self.enrichment_services.get('securitytrails')
+            self.virustotal = self.enrichment_services.get('virustotal')
+            self.shodan = self.enrichment_services.get('shodan')
+            self.alienvault = self.enrichment_services.get('alienvault')
+            self.bgp_ranking = self.enrichment_services.get('bgp_ranking')
+            self.urlhaus = self.enrichment_services.get('urlhaus')
 
-        # TODO: add checks
+            if any(service is None for service in (self.abuseipdb, self.threatfox, self.securitytrails, self.virustotal, self.shodan, self.alienvault, self.bgp_ranking, self.urlhaus)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain complete settings for enrichment services ...")
+                logging.error(f"The configuration file does not contain complete settings for enrichment services")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Enrichment services are not present in the configuration file ...")
+            logging.error(f"Enrichment services are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
+        # NOTE: maybe this option will not be implementd ... 
         self.arguments = config.get('arguments')
 
-        # TODO: add checks
-        self.sniffing = config.get('sniffing') 
-        self.interface = self.sniffing.get('interface')
-        self.filter = self.sniffing.get('filter')
-        self.timeout = self.sniffing.get('timeout')
+        self.sniffing = config.get('sniffing')
+        if self.sniffing:
+            self.interface = self.sniffing.get('interface')
+            self.filter = self.sniffing.get('filter')   # optional
+            self.timeout = self.sniffing.get('timeout')
+            self.filename = self.sniffing.get('filename')
 
-        # TODO: add checks
+            if any(setting is None for setting in (self.interface, self.timeout, self.filename)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain complete settings for packet sniffing ...")
+                logging.error(f"The configuration file does not contain complete settings for packet sniffing")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Sniffing settings are not present in the configuration file ...")
+            logging.error(f"Sniffing settings are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
         self.thresholds = config.get('thresholds')
-        self.MAX_FREQUENCY = self.thresholds.get('MAX_FREQUENCY')
-        self.MAX_DURATION = self.thresholds.get('MAX_DURATION')
-        self.MAX_HTML_SIZE = self.thresholds.get('MAX_HTML_SIZE')
-        self.MAX_SUBDOMAIN_LENGTH = self.thresholds.get('MAX_SUBDOMAIN_LENGTH')
+        if self.thresholds:
+            self.MAX_FREQUENCY = self.thresholds.get('MAX_FREQUENCY')
+            self.MAX_DURATION = self.thresholds.get('MAX_DURATION')
+            self.MAX_HTML_SIZE = self.thresholds.get('MAX_HTML_SIZE')
+            self.MAX_SUBDOMAIN_LENGTH = self.thresholds.get('MAX_SUBDOMAIN_LENGTH')
 
-        # check is not required, because plugins are optional
+            if any(thresholds is None for thresholds in (self.MAX_FREQUENCY, self.MAX_DURATION, self.MAX_HTML_SIZE, self.MAX_SUBDOMAIN_LENGTH)):
+                print(f"[{time.strftime('%H:%M:%S')}] [ERROR] The configuration file does not contain complete threshold settings ...")
+                logging.error(f"The configuration file does not contain complete threshold settings")
+                print("\nExiting program ...\n")
+                sys.exit(1)
+        else:
+            print(f"[{time.strftime('%H:%M:%S')}] [ERROR] Thresholds are not present in the configuration file ...")
+            logging.error(f"Thresholds are not present in the configuration file")
+            print("\nExiting program ...\n")
+            sys.exit(1)
+
+        # plugins are optional
         self.plugins = config.get('plugins')
 
     def print_config(self):
@@ -70,4 +146,3 @@ class AnalystProfile:
         print(f"MAX_HTML_SIZE: {self.MAX_HTML_SIZE}")
         print(f"MAX_SUBDOMAIN_LENGTH: {self.MAX_SUBDOMAIN_LENGTH}")
         print(f"plugins: {self.plugins}")
-
